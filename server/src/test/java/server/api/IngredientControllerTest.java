e09c57f7602cf592872a3b7eca5c9885ea6ec367
Yanick Rietveld <y.r.rietveld@student.tudelft.nl>
2025-11-25 18:20:44 +0100

    Adding tests for IngredientController

--- server/src/test/java/server/api/IngredientControllerTest.java
+++ server/src/test/java/server/api/IngredientControllerTest.java
@@ -3,7 +3,7 @@ package server.api;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
-import server.temp.Recipe;
+import server.temp.Ingredient;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -13,139 +13,139 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
 
-public class RecipeControllerTest {
+public class IngredientControllerTest {
 
-    private RecipeController rc;
-    private Recipe r1;
-    private Recipe r2;
+    private IngredientController ic;
+    private Ingredient i1;
+    private Ingredient i2;
 
     @BeforeEach
     public void setup() {
-        rc = new RecipeController();
-        r1 = new Recipe(0, "Spaghetti");
-        r2 = new Recipe(1, "Pasta");
+        ic = new IngredientController();
+        i1 = new Ingredient(0, "Salt");
+        i2 = new Ingredient(1, "Pepperoni");
     }
 
     @Test
-    public void addNullRecipe() {
-        ResponseEntity<Recipe> actual = rc.add(null);
+    public void addNullIngredient() {
+        ResponseEntity<Ingredient> actual = ic.add(null);
         assertEquals(BAD_REQUEST, actual.getStatusCode());
     }
 
     @Test
     public void checkAdd() {
-        rc.add(r1);
-        rc.add(r2);
-        assertTrue(rc.getAll().containsAll(Arrays.asList(r1, r2)));
+        ic.add(i1);
+        ic.add(i2);
+        assertTrue(ic.getAll().containsAll(Arrays.asList(i1, i2)));
     }
 
     @Test
-    public void checkAddRecipeNameNull() {
-        ResponseEntity<Recipe> response = rc.add(new Recipe(-1, null));
+    public void checkAddIngredientNameNull() {
+        ResponseEntity<Ingredient> response = ic.add(new Ingredient(-1, null));
         assertEquals(BAD_REQUEST, response.getStatusCode());
     }
 
     @Test
-    public void checkNullRecipeName() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void checkNullIngredientName() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         assertFalse(isValidName(null));
     }
 
     @Test
-    public void checkEmptyRecipeName() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void checkEmptyIngredientName() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         assertFalse(isValidName(""));
     }
 
     @Test
-    public void checkGoodRecipeName() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void checkGoodIngredientName() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         assertTrue(isValidName("Spaghetti"));
     }
 
     @Test
-    public void checkRecipeExists() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
-        assertFalse(recipeExists(r1.getId()));
-        rc.add(r1);
-        assertTrue(recipeExists(r1.getId()));
+    public void checkIngredientExists() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        assertFalse(ingredientExists(i1.getId()));
+        ic.add(i1);
+        assertTrue(ingredientExists(i1.getId()));
     }
 
     @Test
     public void checkAutoNewID() {
-        Recipe newRecipe = new Recipe(-1, "Wraps");
+        Ingredient newIngredient = new Ingredient(-1, "Wraps");
 
-        ResponseEntity<Recipe> response = rc.add(newRecipe);
-        Recipe recipe = response.getBody();
+        ResponseEntity<Ingredient> response = ic.add(newIngredient);
+        Ingredient ingredient = response.getBody();
 
         assertEquals(OK, response.getStatusCode());
-        assertNotNull(recipe);
+        assertNotNull(ingredient);
 
-        assertNotEquals(newRecipe.getId(), recipe.getId());
+        assertNotEquals(newIngredient.getId(), ingredient.getId());
     }
 
     @Test
-    public void checkAutoNewIDWithNonEmptyRecipeList() {
-        rc.add(r2);
-        rc.add(r1);
+    public void checkAutoNewIDWithNonEmptyIngredientList() {
+        ic.add(i2);
+        ic.add(i1);
 
-        Recipe newRecipe = new Recipe(-1, "Wraps");
+        Ingredient newIngredient = new Ingredient(-1, "Milk");
 
-        ResponseEntity<Recipe> response = rc.add(newRecipe);
-        Recipe recipe = response.getBody();
+        ResponseEntity<Ingredient> response = ic.add(newIngredient);
+        Ingredient ingredient = response.getBody();
 
         assertEquals(OK, response.getStatusCode());
-        assertNotNull(recipe);
+        assertNotNull(ingredient);
 
-        assertNotEquals(newRecipe.getId(), recipe.getId());
+        assertNotEquals(newIngredient.getId(), ingredient.getId());
     }
 
     @Test
     public void checkRemove() {
-        rc.add(r1);
+        ic.add(i1);
 
-        ResponseEntity<Recipe> response = rc.remove(r1);
+        ResponseEntity<Ingredient> response = ic.remove(i1);
         assertEquals(OK, response.getStatusCode());
     }
 
     @Test
     public void checkRemoveWithMultipleEntries() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
-        rc.add(r1);
-        rc.add(r2);
+        ic.add(i1);
+        ic.add(i2);
 
-        ResponseEntity<Recipe> response = rc.remove(r1);
+        ResponseEntity<Ingredient> response = ic.remove(i1);
         assertEquals(OK, response.getStatusCode());
 
-        assertTrue(recipeExists(r2.getId()));
+        assertTrue(ingredientExists(i2.getId()));
     }
 
     @Test
     public void checkRemoveNull() {
-        ResponseEntity<Recipe> response = rc.remove(null);
+        ResponseEntity<Ingredient> response = ic.remove(null);
         assertEquals(BAD_REQUEST, response.getStatusCode());
     }
 
     @Test
     public void checkRemoveNonExisting() {
-        ResponseEntity<Recipe> response = rc.remove(r1);
+        ResponseEntity<Ingredient> response = ic.remove(i1);
         assertEquals(BAD_REQUEST, response.getStatusCode());
     }
 
     /**
      * Get the private method isValidName for testing
-     * @param name input for isValidName method of RecipeController
+     * @param name input for isValidName method of IngredientController
      * @return true, if valid name
      */
     private boolean isValidName(String name) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Method m = rc.getClass().getDeclaredMethod("isValidName", String.class);
+        Method m = ic.getClass().getDeclaredMethod("isValidName", String.class);
         m.setAccessible(true);
-        return (boolean) m.invoke(rc, name);
+        return (boolean) m.invoke(ic, name);
     }
 
     /**
-     * Get the private method recipeExists for testing
-     * @param recipeID input for recipeExists method of RecipeController
-     * @return true, if recipe with id exists
+     * Get the private method ingredientExists for testing
+     * @param ingredientID input for ingredientExists method of IngredientController
+     * @return true, if ingredient with id exists
      */
-    private boolean recipeExists(int recipeID) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Method m = rc.getClass().getDeclaredMethod("recipeExists", int.class);
+    private boolean ingredientExists(int ingredientID) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method m = ic.getClass().getDeclaredMethod("ingredientExists", int.class);
         m.setAccessible(true);
-        return (boolean) m.invoke(rc, recipeID);
+        return (boolean) m.invoke(ic, ingredientID);
     }
 }
