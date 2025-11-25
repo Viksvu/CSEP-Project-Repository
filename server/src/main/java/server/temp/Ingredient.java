691a1337768f9fbaa6ced5ddb5374b041a896733
Yanick Rietveld <y.r.rietveld@student.tudelft.nl>
2025-11-25 18:21:22 +0100

    Remove currently unused functions of temporary Ingredient class

--- server/src/main/java/server/temp/Ingredient.java
+++ server/src/main/java/server/temp/Ingredient.java
@@ -5,27 +5,20 @@ package server.temp;
 
 public class Ingredient {
     private int id;
-    private int quantity;
     private String name;
 
     /**
      * Create a new ingredient
      * @param id id of ingredient
-     * @param quantity quantity of ingredient
      * @param name name of ingredient
      */
-    public Ingredient(int id, int quantity, String name) {
+    public Ingredient(int id, String name) {
         this.id = id;
-        this.quantity = quantity;
         this.name = name;
     }
 
     public int getId() {
         return id;
-    }
-
-    public int getQuantity() {
-        return quantity;
     }
 
     public String getName() {
