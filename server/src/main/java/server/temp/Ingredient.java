080fc8a3e03e46ae7db7faaf18b5f16f792e33cf
Yanick Rietveld <y.r.rietveld@student.tudelft.nl>
2025-11-25 17:08:12 +0100

    Adding temp Ingredient / Recipe classess for testing

--- server/src/main/java/server/temp/Ingredient.java
+++ server/src/main/java/server/temp/Ingredient.java
@@ -1,4 +1,34 @@
+/**
+ * TODO: NOTE: Class will most likely be replaced by a class out of commons
+ */
 package server.temp;
 
 public class Ingredient {
+    private int id;
+    private int quantity;
+    private String name;
+
+    /**
+     * Create a new ingredient
+     * @param id id of ingredient
+     * @param quantity quantity of ingredient
+     * @param name name of ingredient
+     */
+    public Ingredient(int id, int quantity, String name) {
+        this.id = id;
+        this.quantity = quantity;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
