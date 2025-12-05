f10b10fdf19a2ee88da878934816cc44756fcc36
Matas Stankevičius <m.stankevicius@student.tudelft.nl>
2025-12-05 17:27:48 +0100

    Adding simple selection for shopping list scene

---
+++ client/src/main/java/client/scenes/ShoppingListCtrl.java
@@ -0,0 +1,61 @@
+package client.scenes;
+
+import client.utils.ServerUtils;
+import jakarta.inject.Inject;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ShoppingListCtrl implements Initializable {
+
+    private final MainCtrl mainCtrl;
+    private final ServerUtils server;
+    @FXML
+    private ListView<String> shoppingListView;
+    /**
+     * Constructor
+     *
+     * @param mainCtrl the controllers of all the panes store
+     *                 the main controller as an object
+     * @param server   communication to server.
+     */
+    @Inject
+    public ShoppingListCtrl(MainCtrl mainCtrl, ServerUtils server) {
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        shoppingListView.setItems(FXCollections.observableArrayList());
+
+        shoppingListView.getItems().addAll("Milk", "Eggs", "Butter");    }
+    /**
+     * Clicking cancel should clear whatever the user entered in
+     * the text field and then show the overview
+     */
+    public void goBack() {
+        mainCtrl.showOverview();
+    }
+
+    /**
+     * Removes selected ingredient
+     */
+    public void removeSelected(){
+
+    }
+
+    /**
+     * Clears the full list
+     */
+    public void clearList(){
+
+    }
+
+
+}
