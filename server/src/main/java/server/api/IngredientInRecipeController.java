f269197815c7edb729c764eb95d07b595c40d3f5
Tushit Jain <T.Jain-3@student.tudelft.nl>
2025-12-16 21:11:10 +0100

    refactored the ingredientsInRecipe controller to use the appropriate db services

--- server/src/main/java/server/api/IngredientInRecipeController.java
+++ server/src/main/java/server/api/IngredientInRecipeController.java
@@ -4,6 +4,7 @@ import commons.IngredientInRecipe;
 import commons.Recipes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import server.services.RecipeService;
 import server.services.TempRecipeService;
 
 import java.util.List;
@@ -11,7 +12,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/recipeingredient")
 public class IngredientInRecipeController {
-    private TempRecipeService recipes = TempRecipeService.get();
+    RecipeService recipeService;
+
+    public IngredientInRecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     /**
      * Get all Ingredients in a Recipe
@@ -20,9 +25,13 @@ public class IngredientInRecipeController {
      */
     @GetMapping("/get")
     public ResponseEntity<List<IngredientInRecipe>> get(@RequestParam long id) {
-        Recipes recipe = recipes.getRecipeById(id);
-        if (recipe == null) return ResponseEntity.badRequest().build();
-
+        if (id == 0) return ResponseEntity.badRequest().build();
+        Recipes recipe;
+        try {
+            recipe = recipeService.getRecipeById(id);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
         return ResponseEntity.ok(recipe.getIngredients());
     }
 
@@ -38,10 +47,14 @@ public class IngredientInRecipeController {
             @RequestBody IngredientInRecipe ingredient) {
         if (ingredient == null) return ResponseEntity.badRequest().build();
 
-        Recipes recipe = recipes.getRecipeById(id);
-        if (recipe == null) return ResponseEntity.badRequest().build();
-
+        Recipes recipe;
+        try {
+            recipe = recipeService.getRecipeById(id);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
         recipe.addIngredient(ingredient);
+        recipeService.addRecipe(recipe);
         return ResponseEntity.ok(ingredient);
     }
 
@@ -57,10 +70,14 @@ public class IngredientInRecipeController {
             @RequestBody IngredientInRecipe ingredient) {
         if (ingredient == null) return ResponseEntity.badRequest().build();
 
-        Recipes recipe = recipes.getRecipeById(id);
-        if (recipe == null) return ResponseEntity.badRequest().build();
-
+        Recipes recipe;
+        try {
+            recipe = recipeService.getRecipeById(id);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
         recipe.removeIngredient(ingredient);
+        recipeService.addRecipe(recipe);
         return ResponseEntity.ok(ingredient);
     }
 }
