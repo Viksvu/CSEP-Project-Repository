8213d0c93412cad896835aee1c6bc5553518d9f0
Tushit Jain <T.Jain-3@student.tudelft.nl>
2025-12-16 21:14:18 +0100

    reformatting to pass checkstyle

--- server/src/main/java/server/api/IngredientInRecipeController.java
+++ server/src/main/java/server/api/IngredientInRecipeController.java
@@ -5,7 +5,6 @@ import commons.Recipes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.services.RecipeService;
-import server.services.TempRecipeService;
 
 import java.util.List;
 
@@ -14,6 +13,11 @@ import java.util.List;
 public class IngredientInRecipeController {
     RecipeService recipeService;
 
+    /**
+     * Public constructor for the controller
+     * Needed for spring boot to take care of the dependency injection
+     * @param recipeService the service needed to do crud operations on recipes
+     */
     public IngredientInRecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
