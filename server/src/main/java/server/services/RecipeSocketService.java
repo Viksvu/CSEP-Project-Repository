77da88e533823b66e4f415eff6351925ae71d88d
Matas Stankevičius <m.stankevicius@student.tudelft.nl>
2026-01-16 22:25:08 +0100

    adding service for recipe socket autowiring

--- server/src/main/java/server/services/RecipeSocketService.java
+++ server/src/main/java/server/services/RecipeSocketService.java
@@ -1,4 +1,18 @@
 package server.services;
 
+import org.springframework.stereotype.Service;
+import server.websocket.RecipeWebSocketHandler;
+
+@Service
 public class RecipeSocketService {
+
+    private final RecipeWebSocketHandler socketHandler;
+
+    public RecipeSocketService(RecipeWebSocketHandler socketHandler) {
+        this.socketHandler = socketHandler;
+    }
+
+    public void recipeUpdated(long recipeId) {
+        socketHandler.notifyRecipeUpdated(recipeId);
+    }
 }
