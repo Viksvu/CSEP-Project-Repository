6709effa48ebc5abe201bd7ac30fbcb158cdfe49
Marc de Wilt <m.a.wilt@student.tudelft.nl>
2026-01-16 12:03:52 +0100

    Calculator for the macros

---
+++ client/src/main/java/client/utils/MacroCalc.java
@@ -0,0 +1,32 @@
+package client.utils;
+
+import commons.IngredientInRecipe;
+
+import java.util.List;
+
+public class MacroCalc {
+
+    public static double totalFat(List<IngredientInRecipe> ingredients) {
+        return ingredients.stream()
+                .mapToDouble(i ->
+                        (i.getQuantity() / 100.0)
+                                * i.getIngredient().getFatPer100g())
+                .sum();
+    }
+
+    public static double totalCarbs(List<IngredientInRecipe> ingredients) {
+        return ingredients.stream()
+                .mapToDouble(i ->
+                        (i.getQuantity() / 100.0)
+                                * i.getIngredient().getCarbsPer100g())
+                .sum();
+    }
+
+    public static double totalProtein(List<IngredientInRecipe> ingredients) {
+        return ingredients.stream()
+                .mapToDouble(i ->
+                        (i.getQuantity() / 100.0)
+                                * i.getIngredient().getProteinPer100g())
+                .sum();
+    }
+}
