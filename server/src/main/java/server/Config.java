88526bb702e7a0a2c53c8532d12d884bd1c91a83
Matas Stankevičius <m.stankevicius@student.tudelft.nl>
2026-01-16 23:01:03 +0100

    last refactor before pushing, deleting old jakarta method calls

--- server/src/main/java/server/Config.java
+++ server/src/main/java/server/Config.java
@@ -20,7 +20,6 @@ import java.util.Random;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 @Profile("!test")
