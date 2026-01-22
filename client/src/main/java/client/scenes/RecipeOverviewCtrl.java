461834f012ff2cdb8131dd0a20121c03aca05060
Matas Stankevičius <m.stankevicius@student.tudelft.nl>
2026-01-22 15:56:41 +0100

    Adding shopping list DI for non directly related to the shopping list classes

--- client/src/main/java/client/scenes/RecipeOverviewCtrl.java
+++ client/src/main/java/client/scenes/RecipeOverviewCtrl.java
@@ -135,27 +135,19 @@ public class RecipeOverviewCtrl implements Initializable {
      * @param mainCtrl main controller.
      */
     @Inject
-    public RecipeOverviewCtrl(MainCtrl mainCtrl, ServerUtils server) {
+    public RecipeOverviewCtrl(MainCtrl mainCtrl, ServerUtils server,
+                              ShoppingList shoppingList) {
         this.mainCtrl = mainCtrl;
-        //this.recipeObservableList = FXCollections.observableArrayList();
-        //splitPaneRefreshButton = new SplitPane();
         this.server = server;
         this.ingredientsData = FXCollections.observableArrayList();
         this.isCloning = false;
         this.isRenaming = false;
         ingredientButtons = new ArrayList<>();
+        this.shoppingList=shoppingList;
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //ObservableList<String> recipeObservableList = mainCtrl.getRecipes();
-        //ObservableList<Recipes> recipeList =
-        // FXCollections.observableArrayList(server.getRecipes());
-        //recipeObservableList.add("Test String 1");
-        // Adding anything to the recipeObservableList
-        // will also add to the ListView of Recipes
-        //recipeListView.setItems(recipeObservableList);
-        //recipeListView.setEditable(true);
         favorites=new HashSet<>();
         setLanguageDropDown(resourceBundle);
         searchField.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
@@ -960,11 +952,6 @@ public class RecipeOverviewCtrl implements Initializable {
 
     public ObservableList<Recipes> getRecipeData () {
         return recipeData;
-    }
-
-
-    public void setShoppingList (ShoppingList shoppingList){
-        this.shoppingList = shoppingList;
     }
 
 
