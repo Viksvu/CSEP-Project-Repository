fde6c9189a1e6a7c87cf02b4a530d65ecd6845cb
Kedar Joshi <k.n.joshi@student.tudelft.nl>
2026-01-23 09:47:06 +0100

    Deleted old files for edit button shopping lsit

--- client/src/main/java/client/scenes/OverviewListCtrl.java
+++ client/src/main/java/client/scenes/OverviewListCtrl.java
@@ -2,7 +2,6 @@ package client.scenes;
 
 import client.EditButton;
 import client.EditButtonOptions;
-import client.EditButtonShoppingList;
 import client.commonsClient.IngredientInShoppingList;
 import client.commonsClient.ShoppingList;
 import client.utils.ServerUtils;
