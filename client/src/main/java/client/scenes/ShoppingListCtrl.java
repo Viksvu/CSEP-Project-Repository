9eb2f7067236b62ac43ca8b18770a097d71e8da8
Matas Stankevičius <m.stankevicius@student.tudelft.nl>
2026-01-23 20:31:54 +0100

    fixing checkstyle errors

--- client/src/main/java/client/scenes/ShoppingListCtrl.java
+++ client/src/main/java/client/scenes/ShoppingListCtrl.java
@@ -56,6 +56,12 @@ public class ShoppingListCtrl implements Initializable {
     @FXML
     private Button downloadShoppingListButton;
 
+    /**
+     * A constructor for the controller
+     * @param mainCtrl
+     * @param server
+     * @param shoppingList
+     */
     @Inject
     public ShoppingListCtrl(MainCtrl mainCtrl,
                             ServerUtils server,
@@ -67,9 +73,7 @@ public class ShoppingListCtrl implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         shoppingListView.setItems(items);
-
         Label emptyLabel = new Label(
                 "Your shopping list is empty\nAdd ingredients to get started"
         );
@@ -79,20 +83,16 @@ public class ShoppingListCtrl implements Initializable {
                 -fx-alignment: center;
                 """);
         shoppingListView.setPlaceholder(emptyLabel);
-
         shoppingListView.setCellFactory(lv -> new ListCell<>() {
-
             @Override
             protected void updateItem(IngredientInShoppingList item, boolean empty) {
                 super.updateItem(item, empty);
-
                 if (empty || item == null) {
                     setText(null);
                     setGraphic(null);
                     setStyle("");
                     return;
                 }
-
                 EditButton<IngredientInShoppingList> deleteButton =
                         new EditButton<>(
                                 item,
@@ -104,7 +104,6 @@ public class ShoppingListCtrl implements Initializable {
                                 EditButtonOptions.REMOVE_INGREDIENT,
                                 shoppingList
                         );
-
                 EditButton<IngredientInShoppingList> editButton =
                         new EditButton<>(
                                 item,
@@ -116,38 +115,28 @@ public class ShoppingListCtrl implements Initializable {
                                 EditButtonOptions.EDIT_INGREDIENT,
                                 shoppingList
                         );
-
                 deleteButton.setDisable(item.isChecked());
                 editButton.setDisable(item.isChecked());
-
                 HBox row = new HBox(8, deleteButton, editButton);
                 row.setPickOnBounds(false);
-
                 setText(item.toString());
                 setGraphic(row);
                 setGraphicTextGap(-80);
                 setTextOverrun(OverrunStyle.ELLIPSIS);
-
                 updateStyle(item);
             }
-
             {
                 setOnMouseClicked(event -> {
                     if (!isEmpty()) {
                         IngredientInShoppingList item = getItem();
-
                         item.setChecked(!item.isChecked());
-
-                        FXCollections.sort(
-                                items,
-                                Comparator.comparing(IngredientInShoppingList::isChecked)
+                        FXCollections.sort(items, Comparator.
+                                comparing(IngredientInShoppingList::isChecked)
                         );
-
                         updateStyle(item);
                     }
                 });
             }
-
             private void updateStyle(IngredientInShoppingList item) {
                 if (item.isChecked()) {
                     setStyle("-fx-strikethrough: true; -fx-text-fill: gray;");
@@ -156,15 +145,17 @@ public class ShoppingListCtrl implements Initializable {
                 }
             }
         });
-
         ImageView iv = new ImageView(
-                new Image(getClass().getResourceAsStream("/pictures/save.png"))
-        );
+                new Image(getClass().getResourceAsStream("/pictures/save.png")));
         iv.setFitWidth(20);
         iv.setFitHeight(20);
         downloadShoppingListButton.setGraphic(iv);
     }
 
+    /**
+     * Updates the language
+     * @param resourceBundle
+     */
     public void updateLanguage(ResourceBundle resourceBundle) {
         shoppingListLabel.setText(resourceBundle.getString("shoppingList.label"));
         shoppingListAddIngredients.setText(resourceBundle.getString("shoppingList.addIngredient"));
@@ -173,10 +164,16 @@ public class ShoppingListCtrl implements Initializable {
         shoppingListClearButton.setText(resourceBundle.getString("shoppingList.clear"));
     }
 
+    /**
+     * Goes back to overview
+     */
     public void goBack() {
         mainCtrl.showOverview();
     }
 
+    /**
+     * Refreshes the page
+     */
     public void refresh() {
         items.setAll(shoppingList.getShoppingList());
         totalItemsLabel.setText(
@@ -184,24 +181,39 @@ public class ShoppingListCtrl implements Initializable {
         );
     }
 
+    /**
+     * Edits an ingredient
+     * @param ingredient
+     */
     public void editIngredient(IngredientInShoppingList ingredient) {
         mainCtrl.showEditIngredient(ingredient);
     }
 
+    /**
+     * Adds an ingredient
+     */
     public void addIngredient() {
         mainCtrl.showAddIngredient();
     }
 
+    /**
+     * Adds a recipe
+     */
     public void addRecipe() {
         mainCtrl.showAddRecipeIngredientsOverview();
     }
 
+    /**
+     * clears the list
+     */
     public void clearList() {
         shoppingList.resetShoppingList();
         items.clear();
         refresh();
     }
-
+    /**
+     * Downloads the shopping list
+     */
     public void downloadShoppingList() {
         mainCtrl.showSaveRecipe(this.shoppingList, this);
     }
